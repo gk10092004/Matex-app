df3cd328cb54f8186d91eb4d54066ce64ee3e4a5
Gaurav Kumar <gaurav23270143@gmail.com>
2024-11-29 10:58:42 +0530

    upadate wrong url handler

--- frontend/src/components/WrongUrl.jsx
+++ frontend/src/components/WrongUrl.jsx
@@ -1,8 +1,8 @@
 const WrongUrl = () => {
   return (
-    <div className="flex w-[100vw] h-[100vh] justify-center items-center " >
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center " >
       <h1 className="text-[5rem] font-bold text-[#395634] " >You are on wrong URL</h1>
-      <h2 className="text-[3rem] font-bold text-[#395634] hover:text-[black] " >Click here for valid url</h2>
+      <h2 className="text-[3rem] font-bold text-[#395634] hover:text-[black] cursor-pointer " >Click here for valid url</h2>
     </div>
   )
 }
